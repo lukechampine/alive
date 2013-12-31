4cd6f5333fde86cde047448bf57982780c964b4d
lukechampine <luke.champine@gmail.com>
2013-12-30 19:15:51 -0500

    increase modularity and change visualization

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-function calcAge(dateString) {
-	var birthday = +new Date(dateString);
-	return ~~((Date.now() - birthday) / (31557600000));
+var canvas, ctx, age, curViz;
+
+// draw remaining years as a column of rectangles
+function drawYears() {
+    // resize canvas
+    canvas.height = 500;
+    canvas.width = 90;
+    ctx.fillStyle = "rgba(200, 0, 0, 0.6)";
+    for (var i = 0; i < age.years; i++)
+    	ctx.fillRect((i % 5) * 20, Math.floor(i/5)*20, 10, 10);
+    ctx.fillStyle = "rgba(0, 0, 200, 0.6)";
+    for (var i = age.years; i < 100; i++)
+		ctx.fillRect((i % 5) * 20, Math.floor(i/5)*20, 10, 10);
 }
 
-function draw() {
-    var canvas = document.getElementById("canvas");
-    var age = calcAge(document.getElementById("dob").value);
-    if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "rgba(200,0,0,0.5)";
-        ctx.fillRect (0, 0, 73, age*5);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (0, age*5, 73, 500);
+// calculate age and store various increments in an object
+function calcAge() {
+    var birthday = +new Date(document.getElementById("dob").value);
+    var s = (Date.now() - birthday) / 1000;
+    age = {
+        seconds: Math.floor(s),
+        days: Math.floor(s / (60*60*24)),
+        weeks: Math.floor(s / (60*60*24*7)),
+        months: Math.floor(s / (60*60*24*30.42)),
+        years: Math.floor(s / (60*60*24*365.25)),
     }
+}
+
+// recalculate age, clear canvas, and call current visualization function
+function update() {
+	calcAge();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	curViz();
+}
+
+// initialize global variables and update
+function main() {
+	canvas = document.getElementById("canvas");
+    if (!canvas.getContext) return;
+    ctx = canvas.getContext("2d");
+    curViz = drawYears;
+    update();
 }
